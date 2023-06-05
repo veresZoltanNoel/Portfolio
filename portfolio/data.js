7b6fcdce04a4efa0c7963bdedb3d5bd24f151d81
Veres Zoltan Noel <veres.zoltan413@gmail.com>
2023-06-05 16:53:51 +0200

    Protfolio site with the first two projects

---
+++ portfolio/data.js
@@ -0,0 +1,114 @@
+export const skillArr = {
+    "web":{
+        "1" :{
+            "name" :"HTML5",
+            "icon" :"html",
+            "color":"#E44D26"
+        },
+        "2" :{
+            "name" :"CSS",
+            "icon" :"css",
+            "color":"#1172B8"
+        },
+        "3" :{
+            "name" :"JavaScript",
+            "icon" :"javascript",
+            "color":"#F7DF1E"
+        },
+        "4" :{
+            "name" :"TailwindCSS",
+            "icon" :"tailwind",
+            "color":"#44A8B3"
+        },
+        "5" :{
+            "name" :"SASS",
+            "icon" :"scss",
+            "color":"#CD6799"
+        },
+        "6" :{
+            "name" :"PHP",
+            "icon" :"php",
+            "color":"#777BB3"
+        },
+        "7" :{
+            "name" :"Bootstrap",
+            "icon" :"bootstrap",
+            "color":"#563D7C"
+        }
+    },
+    "design":{
+        "1":{
+            "name" :"PhotoShop",
+            "icon" :"photoshop",
+            "color":"#001E36"
+        },
+        "2":{
+            "name" :"Illsutrator",
+            "icon" :"illustrator",
+            "color":"#330000"
+        },
+        "3":{
+            "name" :"InDesign",
+            "icon" :"indesign",
+            "color":"#49021F"
+        },
+        "4":{
+            "name" :"XD",
+            "icon" :"xd",
+            "color":"#470137"
+        },
+        "5":{
+            "name" :"Figma",
+            "icon" :"figma",
+            "color":"#F24E1E"
+        }
+    },
+    "other":{
+        "1":{
+            "name" :"C#",
+            "icon" :"csharp",
+            "color":"#A179DC"
+        },
+        "2":{
+            "name" :"Github",
+            "icon" :"github",
+            "color":"#000000"
+        },
+        "3":{
+            "name" :"VSCode",
+            "icon" :"vscode",
+            "color":"#0065A9"
+        },
+        "4":{
+            "name" :"Google",
+            "icon" :"google",
+            "color":"#34A853"
+        }
+    }
+}
+export const projectsArr = [
+    {
+        title: "Calculator App",
+        image: "./imgs/calculator_img.webp",
+        description: "built with HTML, CSS, and JS. Perform arithmetic calculations with error handling, history tracking, and persistent storage.",
+        link: "./projects/Calculator/index.html"
+    },
+    {
+        title: "To-Do App",
+        image: "./imgs/toDo_img.webp",
+        description: "built with HTML, CSS, and JS. Create, track, and delete tasks easily. Persistent storage using localStorage.",
+        link: "./projects/ToDo/index.html"
+    },
+    {
+        title: "Project 3",
+        image: "./imgs/bg3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        link: "./projects/Calculator/index.html"
+    },
+    {
+        title: "Project 4",
+        image: "./imgs/bg4.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        link: "./projects/Calculator/index.html"
+    }
+];
---
+++ portfolio/script.js
@@ -0,0 +1,147 @@
+
+import { skillArr } from './data.js';
+import { projectsArr } from './data.js';
+
+//Skill Section content
+const webSKillContainer = document.getElementById("web")
+const designSKillContainer = document.getElementById("design")
+const otherSKillContainer = document.getElementById("other")
+
+function createSkillElement (skill) {
+    const container = document.createElement("div");
+    const iconContainer = document.createElement("div");
+    const icon = document.createElement("img");
+    const title = document.createElement("h4");
+
+    container.classList.add('skill', 'flex', 'flex-col', 'items-center', 'gray')
+
+    iconContainer.classList.add('icon-container')
+    iconContainer.style.borderColor = skill.color;
+
+    icon.className ="w-8 h-8"
+    icon.title = `${skill.name} icon`
+    icon.src = `./imgs/icon/${skill.icon}.svg`;
+
+    title.textContent = skill.name
+    title.style.color = skill.color
+
+    container.appendChild(iconContainer)
+    iconContainer.appendChild(icon)
+    container.appendChild(title)
+
+    return container
+}
+
+for (const category in skillArr) {
+    const subSkills = skillArr[category];
+    for (const skillId in subSkills) {
+        const skill = subSkills[skillId];
+        //append to dom when the category if the statement is true 
+        switch (category) {
+            case "web":
+                webSKillContainer.appendChild(createSkillElement(skill));
+                break;
+            case "design":
+                designSKillContainer.appendChild(createSkillElement(skill));
+                break;
+            case "other":
+                otherSKillContainer.appendChild(createSkillElement(skill));
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+const skills = document.querySelectorAll('.skill');
+function removeGrayscale() {
+  //Random 1 - 4
+  const elementsToChange = Math.floor(Math.random() * 4) + 1;
+  
+  //Shuffle the array of skills
+  const shuffledSkillElements = Array.from(skills).sort(() => 0.5 - Math.random());
+  
+  //Remove grayscale from "elementsToChange"-> elements = colored
+  for (let i = 0; i < elementsToChange; i++) {
+    shuffledSkillElements[i].classList.remove('gray');
+    
+    //Add the "scale" class to the colored els
+    if (!shuffledSkillElements[i].classList.contains('gray')) {
+      shuffledSkillElements[i].classList.add('scale');
+    }
+  }
+  
+  //Add back to the grayscale to the remaining elements
+  for (let i = elementsToChange; i < skills.length; i++) {
+    shuffledSkillElements[i].classList.add('gray');
+    shuffledSkillElements[i].classList.remove('scale');
+  }
+} setInterval(removeGrayscale, 1000);
+
+
+//Mobile navigation
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+    const mobileNav = document.getElementById('mobileNav');
+  
+    mobileMenuBtn.addEventListener('click', function () {
+      mobileNav.classList.toggle('hidden');
+    });
+});
+
+
+//Projects section content
+const projectGrid = document.querySelector('.project-grid');
+function createProjectCard(project) {
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardBody = document.createElement('div');
+  const title = document.createElement('h3');
+  const description = document.createElement('p');
+
+
+
+  card.classList.add('project-card', 'rounded-lg', 'overflow-hidden', 'shadow-lg', 'w-3/4','cursor-pointer');
+
+  image.classList.add('w-full');
+  image.src = project.image;
+  image.alt = project.title;
+  image.style.filter = 'grayscale(100%)';
+  image.style.transition = 'filter .7s ease';
+
+  cardBody.classList.add('p-4');
+
+  title.classList.add('text-xl', 'font-bold');
+  title.textContent = project.title;
+
+  description.classList.add('text-gray-700', 'mt-2');
+  description.textContent = project.description;
+
+  card.appendChild(image);
+  card.appendChild(cardBody);
+  cardBody.appendChild(title);
+  cardBody.appendChild(description);
+
+  // Add hover effect to remove grayscale
+  card.addEventListener('mouseenter', () => {
+    image.style.filter = 'grayscale(0)';
+  });
+
+  // Reset grayscale on mouse leave
+  card.addEventListener('mouseleave', () => {
+    image.style.filter = 'grayscale(100%)';
+  });
+
+  card.addEventListener('click', () => {
+    window.open(project.link, '_blank');
+  });
+
+  return card;
+}
+
+
+projectsArr.forEach((project) => {
+  const card = createProjectCard(project);
+  projectGrid.appendChild(card);
+});
+
