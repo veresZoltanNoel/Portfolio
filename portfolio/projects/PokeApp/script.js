612c42b0c1a0872f0ef067da0632ed14baff2462
Zoli <veres.zoltan413@gmail.com>
2023-06-08 19:44:06 +0200

    type color combination for  element + comment

--- portfolio/projects/PokeApp/script.js
+++ portfolio/projects/PokeApp/script.js
@@ -1,26 +1,14 @@
+// Define the base URL of the PokeAPI
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
 
-function hexToRgb(hex) {
-  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  const r = parseInt(result[1], 16);
-  const g = parseInt(result[2], 16);
-  const b = parseInt(result[3], 16);
-  return `rgb(${r}, ${g}, ${b})`;
-}
-
-function mixColors(color1, color2) {
-  const r = Math.round((color1.r + color2.r) / 2);
-  const g = Math.round((color1.g + color2.g) / 2);
-  const b = Math.round((color1.b + color2.b) / 2);
-  return `rgb(${r}, ${g}, ${b})`;
-}
 // Fetch data from the base URL
 fetch(baseUrl)
   .then(response => {
+    // Check if the response is not okay
     if (!response.ok) {
       throw new Error("Request Error");
     }
+    // Convert the response to JSON format
     return response.json();
   })
   .then(data => {
@@ -29,19 +17,27 @@ fetch(baseUrl)
 
     // Function to fetch and process data for each Pokémon URL
     const fetchPokemonData = (url) => {
+      // Fetch data from the specified URL
       fetch(url)
         .then(response => {
+          // Check if the response is not okay
           if (!response.ok) {
             throw new Error("Request Error");
           }
+          // Convert the response to JSON format
           return response.json();
         })
         .then(pokemonData => {
           // Process the data for each Pokémon
+
+          // Get the container element to hold Pokémon information
           const pokemonsContainer = document.getElementById('pokemons');
+
+          // Extract the Pokémon's name and image URL from the data
           const pokemonName = pokemonData.name;
           const pokemonImageUrl = pokemonData.sprites.front_default;
 
+          // Check if the Pokémon has an image URL
           if (pokemonImageUrl) {
             // Create HTML elements to display Pokémon name, image, and stats
             const container = document.createElement('div');
@@ -52,8 +48,9 @@ fetch(baseUrl)
             const hpText = document.createElement("span");
             const hpValue = document.createElement("span");
             const typeImgContainer = document.createElement("div");
-
             const cardTopDesign = document.createElement("div");
+
+            // Set the styles for the cardTopDesign element
             cardTopDesign.style.position = "absolute";
             cardTopDesign.style.top = "0";
             cardTopDesign.style.width = "100%";
@@ -61,77 +58,115 @@ fetch(baseUrl)
             cardTopDesign.style.borderRadius = "0 0 100% 100%";
             cardTopDesign.style.zIndex = "-1";
 
-            container.classList.add('poke-container','relative','flex', 'flex-col', 'items-center','py-12','bg-gray-100');
+            // Add classes to the container element
+            container.classList.add('poke-container', 'relative', 'flex', 'flex-col', 'items-center', 'py-12', 'bg-gray-100');
+
+            // Add classes and content to the hpDiv element
             hpDiv.classList.add("px-2", "py-1", "rounded-full", 'flex', 'flex-row', 'gap-2', 'bg-gray-400');
             hpText.classList.add("block");
             hpValue.classList.add("block");
             hpText.textContent = pokemonData.stats[0].stat.name;
             hpValue.textContent = pokemonData.stats[0].base_stat;
+
+            // Add classes and content to the name element
             name.classList.add('font-bold', 'text-lg');
-            statContainer.classList.add('flex', 'flex-row', 'gap-4', 'text-center', 'p-2');
             name.textContent = pokemonName;
+
+            // Set the image source and alt attribute
             image.src = pokemonImageUrl;
             image.alt = pokemonName;
+
+            // Add classes to the typeImgContainer element
             typeImgContainer.classList.add('grid', 'gap-4');
             pokemonData.types.length === 1 ? typeImgContainer.classList.add('grid-cols-1') : typeImgContainer.classList.add('grid-cols-2');
 
+            // Append child elements to the hpDiv element
             hpDiv.appendChild(hpText);
             hpDiv.appendChild(hpValue);
-            container.appendChild(hpDiv)
+
+            // Append child elements to the container element
+            container.appendChild(hpDiv);
             container.appendChild(name);
             container.appendChild(image);
 
-            // Loop through the stats of the Pokémon and create elements to display 3 of the stats
+            // Loop through the stats of the Pokémon and create elements for 3 of the stats
             for (const { stat, base_stat } of pokemonData.stats) {
               if (!['special-attack', 'special-defense', 'hp'].includes(stat.name)) {
                 const statElement = document.createElement('div');
                 const statName = document.createElement('span');
                 const statValue = document.createElement('span');
 
+                // Add classes and content to the statElement
                 statElement.classList.add('grid', 'grid-rows-2');
                 statName.textContent = stat.name;
                 statValue.textContent = base_stat;
+
+                // Add classes to the statName and statValue elements
                 statName.classList.add('block');
                 statValue.classList.add('block', 'font-bold');
 
+                // Append child elements to the statElement
                 statElement.appendChild(statValue);
                 statElement.appendChild(statName);
+
+                // Append the statElement to the statContainer
                 statContainer.appendChild(statElement);
               }
             }
+
+            // Create an array to store the Pokémon types
             let typeCount = [];
+
+            // Loop through the types of the Pokémon
             for (const { type } of pokemonData.types) {
+              // Add the type name to the typeCount array
               typeCount.push(type.name);
 
+              // Create an image element for the type
               const typeImg = document.createElement('img');
               typeImg.style.width = '25px';
               typeImg.src = `pokemonTypes/${type.name}.png`;
+
+              // Add a class to the name element based on the type
               name.classList.add(`type-${type.name}`);
+
+              // Append the typeImg to the typeImgContainer
               typeImgContainer.appendChild(typeImg);
+
+              // Set the background color of cardTopDesign based on the type count
               if (typeCount.length === 1) {
-                cardTopDesign.style.backgroundColor= `var(--${type.name}-color)`
-              }
-              else{
-                let color1 = hexToRgb(getComputedStyle(document.documentElement).getPropertyValue(`--${typeCount[0]}-color`));
-                let color2 = hexToRgb(getComputedStyle(document.documentElement).getPropertyValue(`--${typeCount[1]}-color`));
-                console.log(`--${typeCount[1]}-color`);
-                const mixedColor  = mixColors(color1, color2);
-                console.log(mixedColor);
-                cardTopDesign.style.backgroundColor= `${mixedColor}`
+                cardTopDesign.style.backgroundColor = `var(--${type.name}-color)`;
+              } else {
+                // Get the color values from CSS variables
+                let color1 = getComputedStyle(document.documentElement).getPropertyValue(`--${typeCount[0]}-color`);
+                let color2 = getComputedStyle(document.documentElement).getPropertyValue(`--${typeCount[1]}-color`);
+
+                // Convert hex colors to RGB format
+                let rgbColor1 = hexToRgb(color1);
+                let rgbColor2 = hexToRgb(color2);
+
+                // Calculate the combined color
+                let combinedColor = mixColors(rgbColor1, rgbColor2);
+
+                // Set the background color of cardTopDesign using the combined color
+                cardTopDesign.style.backgroundColor = `rgb(${combinedColor.r}, ${combinedColor.g}, ${combinedColor.b})`;
               }
             }
 
-            container.appendChild(typeImgContainer)
+            // Append child elements to the container element
+            container.appendChild(typeImgContainer);
             container.appendChild(statContainer);
-            container.appendChild(cardTopDesign)
-            pokemonsContainer.appendChild(container);
+            container.appendChild(cardTopDesign);
 
+            // Append the container element to the pokemonsContainer
+            pokemonsContainer.appendChild(container);
           }
         })
         .catch(error => {
           console.log(error);
         });
     };
+
     // Fetch and process data for each Pokémon URL
     pokemonUrls.forEach(url => {
       fetchPokemonData(url);
@@ -141,4 +176,23 @@ fetch(baseUrl)
     console.log(error);
   });
 
+// Utility function to convert a hexadecimal color code to RGB format
+function hexToRgb(hex) {
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (result) {
+    const r = parseInt(result[1], 16);
+    const g = parseInt(result[2], 16);
+    const b = parseInt(result[3], 16);
+    return { r, g, b };
+  }
+  return null;
+}
 
+// Utility function to mix two RGB colors
+function mixColors(color1, color2) {
+  const r = Math.round((color1.r + color2.r) / 2);
+  const g = Math.round((color1.g + color2.g) / 2);
+  const b = Math.round((color1.b + color2.b) / 2);
+  return { r, g, b };
+}
